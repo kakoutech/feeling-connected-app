d96b0f8f44f9497a3e1258a611e6e5eb6fe62758
rafimashood <mashood.rafi@codingcops.com>
2025-01-07 14:30:27 +0500

    code refactored

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
